f96785e40c0151e5f11be0e078544fb2ab42b54c
Noor <noormir1295@gmail.com>
2020-11-02 21:00:41 -0500

    deploying

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget-tracker", {
+mongoose.connect("mongodb://localhost/Budget", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
